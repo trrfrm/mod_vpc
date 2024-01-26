55f9afd46840f3ab47a3b500392ba0f76721b09d
nykzaR <nykzar@outlook.com>
2024-01-26 23:09:24 +0530

    added changes

--- examples/main.tf
+++ examples/main.tf
@@ -1,28 +1,25 @@
 module "mod_vpc" {
 
-    source          = "git::https://github.com/trrfrm/mod_vpc.git"
-    default_details = {
-      name          = "from-tf"
-      region        = "ap-south-2"
+    source              = "git::https://github.com/trrfrm/mod_vpc.git"
+    default_details     = {
+      name              = "from-tf"
+      region            = "ap-south-2"
     }
-    vpc_details     = {
-      name          = "vpc"
-      cidr_block    = "10.0.0.0/16"
+    vpc_details         = {
+      name              = "vpc"
+      cidr_block        = "10.0.0.0/16"
     }
-    subnet_tags {
-      name          = [ "web1", "web2" ]
+    subnet_tags         = {
+      name              = [ "web1", "web2" ]
     }
-    subnet_details {
-      availability_zone = [ "ap-south-2a", "ap-south-2c" ]
-    }
-    webserver_info {
-        name                    = "webservers"
-        key_name                = "deployment"
-        instance_type           = "t3.micro"
-        public_ip_enabled       = true
+    webserver_info      = {
+      name              = "webservers"
+      key_name          = "deployment"
+      instance_type     = "t3.micro"
+      public_ip_enabled = true
     }
 }
 
 output "total_subnets" {
-    value           = module.mod_vpc.subnet_count
+    value               = module.mod_vpc.subnet_count
 }
